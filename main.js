63c48b16e04dd11616d2abe210bc5d4cb88eaa82
BlockDev <bisimwad6@gmail.com>
2026-09-05 21:20:15 +0100

    create a reverse function for bytes

--- main.js
+++ main.js
@@ -26,12 +26,34 @@
 //ctors for constructors
 let ctors;
 
+// reverse function
+// bbbbbbbb aaaaaaaa
+// 01011100 01011100
+// 11111111 00000000
+//          01011100
+
+// >>8
+let rev;
+rev = val => {
+  let a, b;
+  let mask;
+  let i;
+
+  mask = 0xff;
+  a = (val & mask);
+  mask = 0xff00;
+  i = (val & mask);
+  b = (i >> 8);
+
+  return String.fromCharCode(a).concat(String.fromCharCode(b))
+}
+
 //ax=b8 bx=bb cx=b9
 ctors = {
-  copy2ax: val => "\xb8",
+  copy2ax: val => "\xb8" + rev(val),
   copy2sp: () => void 0,
-  copy2bx: val => void 0,
-  copy2cx: val => void 0,
+  copy2bx: val => "\xbb" + rev(val),
+  copy2cx: val => "\xb9" + rev(val),
   biosinterrupt: () => void 0,
   interruptoff: () => void 0,
   halt: () => void 0,
@@ -39,3 +61,18 @@ ctors = {
   padding: amt => void 0,
   magicstr: () => void 0
 };
+
+let x, y, z;
+
+x = 0xccdd;
+y = ctors.copy2ax(x);
+
+console.log(y.length)
+// z = y.split('')
+//   .map(a =>
+//     a.charCodeAt(0)
+//   );
+//
+// console.log(z);
+
+
